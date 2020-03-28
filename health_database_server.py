03a3b11a88c82c37f7dc7a1aa24dfc2789f2939f
nbelisle <nicolebelisle121@gmail.com>
2020-03-28 19:50:25 -0400

    added procudure to initialize database

--- health_database_server.py
+++ health_database_server.py
@@ -5,5 +5,17 @@ db = []
 app = Flask(__name__)
 
 
+def add_patient_to_db(name, id, age):
+    new_patient = ("name": name, "id": id, "age": age)
+    db.append(new_patient)
+    return True
+
+
+def init_database():
+    add_patient_to_db("Ann Ables", 1010, 35)
+    add_patient_to_db("Bob Boyles", 102, 40)
+    # Add the Code to start the logging
+
 if __name__ == '__main__':
+    init_database
     app.run()
