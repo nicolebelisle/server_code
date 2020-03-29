b9e6a1ed9457f4b82faf8dbcf79a32af9619cd4a
nbelisle <nicolebelisle121@gmail.com>
2020-03-29 11:11:24 -0400

    Wrote test functions for server

--- health_database_server.py
+++ health_database_server.py
@@ -6,44 +6,89 @@ app = Flask(__name__)
 
 
 def add_patient_to_db(name, id, age):
-    new_patient = {"name": name, "id": id, "age": age}
+    new_patient = {"name": name, "id": id, "age": age, "tests": []}
     db.append(new_patient)
+    print("db is {}".format(db))
     return True
 
 
 def init_database():
-    add_patient_to_db("Ann Ables", 1010, 35)
+    add_patient_to_db("Ann Ables", 101, 35)
     add_patient_to_db("Bob Boyles", 102, 40)
     # Add the Code to start the logging
 
 
 @app.route("/new_patient", methods =["POST"])
 def post_new_patient():
-    """
-    Recieve the posting json
-    Verify the json contains the correct keys and data
-    If data is bad, reject request with bad status to client
-    If data is good, add patient to database
-    return good status to client
-    """
-
     in_dict = request.get_json()
-        check_result = verify_new_patient_info(in_dict)
-        if _check_result is not True:
-            return check_result, 400 
-        add_patient_to_db(in_dict["name"], in_dict["id"], in_dict["age"])
-        return "Patient added", 200
+    check_result = verify_new_patient_info(in_dict)
+    if check_result is not True:
+        return check_result, 400 
+    add_patient_to_db(in_dict["name"], in_dict["id"], in_dict["age"])
+    return "Patient added", 200
 
 def verify_new_patient_info(in_dict):
     expected_keys = ("name", "id", "age")
     expected_types = (str, int, int)
-    for i, key in enumerate(expected_keys)
-    if key not in in_dict.keys():
-        return "{} key not found".format(key)
-    if type(in_dict[key]) is not expected_types[i]:
-        return "{} value not correct type".format(key)
+    for i, key in enumerate(expected_keys):
+        if key not in in_dict.keys():
+            return "{} key not found".format(key)
+        if type(in_dict[key]) is not expected_types[i]:
+            return "{} value not correct type".format(key)
+    return True
+
+
+@app.route("/add_test", methods =["POST"])
+def post_add_test():
+    """
+    Recieve the posting json
+    Verify the json contains the correct keys and data
+    Verify that the patient id exists in database
+    If data is bad, reject request with bad status to client
+    If data is good, add patient to database
+    return good status to client
+    """
+    in_dict = request.get_json()
+    check_result = verify_add_test_info(in_dict)
+    if check_result is not True:
+        return check_result, 400
+    if is_patient_in_db(in_dict["id"]) is False:
+        return "Patient {} is not found on sever".format(in_dict["id"]), 
+    add_result = add_test_to_patient(in_dict)
+    if add_result:
+        return "Test added to patient od {}".format(in_dict["id"]), 200
+    else:
+        return "Unknown problem", 400
     
-    
+
+
+def verify_add_test_info(in_dict):
+    expected_keys = ("id", "test_name", "test_result")
+    expected_types = (int, str, int)
+    for i, key in enumerate(expected_keys):
+        if key not in in_dict.keys():
+            return "{} key not found".format(key)
+        if type(in_dict[key]) is not expected_types[i]:
+            return "{} value not correct type".format(key)
+    return True
+
+
+def is_patient_in_db(id):
+    for patient in db:
+        if patient["id"] == id:
+            return True
+    return False
+
+
+def add_test_to_patient(in_dict):
+    for patient in db:
+        if patient["id"] == in_dict["id"]:
+            patient["tests"].append((in_dict["test_name"],
+                                    in_dict["test_result"]))
+            print("db is {}".format(db))
+            return True
+    return False
+
 
 if __name__ == '__main__':
     init_database
