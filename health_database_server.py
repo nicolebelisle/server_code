1a7c760c9eb2942e7b2b6378110c280d2562e6e2
nbelisle <nicolebelisle121@gmail.com>
2020-03-28 20:09:43 -0400

    Added our new patient route

--- health_database_server.py
+++ health_database_server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 def add_patient_to_db(name, id, age):
-    new_patient = ("name": name, "id": id, "age": age)
+    new_patient = {"name": name, "id": id, "age": age}
     db.append(new_patient)
     return True
 
@@ -15,6 +15,35 @@ def init_database():
     add_patient_to_db("Ann Ables", 1010, 35)
     add_patient_to_db("Bob Boyles", 102, 40)
     # Add the Code to start the logging
+
+
+@app.route("/new_patient", methods =["POST"])
+def post_new_patient():
+    """
+    Recieve the posting json
+    Verify the json contains the correct keys and data
+    If data is bad, reject request with bad status to client
+    If data is good, add patient to database
+    return good status to client
+    """
+
+    in_dict = request.get_json()
+        check_result = verify_new_patient_info(in_dict)
+        if _check_result is not True:
+            return check_result, 400 
+        add_patient_to_db(in_dict["name"], in_dict["id"], in_dict["age"])
+        return "Patient added", 200
+
+def verify_new_patient_info(in_dict):
+    expected_keys = ("name", "id", "age")
+    expected_types = (str, int, int)
+    for i, key in enumerate(expected_keys)
+    if key not in in_dict.keys():
+        return "{} key not found".format(key)
+    if type(in_dict[key]) is not expected_types[i]:
+        return "{} value not correct type".format(key)
+    
+    
 
 if __name__ == '__main__':
     init_database
